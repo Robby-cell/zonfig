6d62ffd00c8c85be616359c835bb8ebddc83f177
Robert Williamson <robert11122002@gmail.com>
2024-02-04 00:18:50 +0000

    added a bit of functionality to the Token struct

--- src/Token.zig
+++ src/Token.zig
@@ -25,4 +25,34 @@ pub const Position = struct {
     start: usize,
     end: usize,
     line: usize,
+
+    pub fn init(start: usize, end: usize, line: usize) Position {
+        return .{
+            .start = start,
+            .end = end,
+            .line = line,
+        };
+    }
 };
+
+pub fn init(token_type: TokenType, position: Position) Self {
+    return .{
+        .type = token_type,
+        .position = position,
+    };
+}
+
+pub fn write(self: *const Self, writer: anytype, buffer: []const u8) anyerror!void {
+    switch (self.type) {
+        .eq => try writer.print("=", .{}),
+        .comma => try writer.print(",", .{}),
+        .string, .int, .float, .ident => try writer.print("{s}", .{buffer[self.position.start..self.position.end]}),
+        else => |b| try writer.print("{c}", .{switch (b) {
+            .left_bracket => '[',
+            .right_bracket => ']',
+            .left_curly => '{',
+            .right_curly => '}',
+            else => unreachable,
+        }}),
+    }
+}
